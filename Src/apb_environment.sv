ef00f25326a11ef1d9cbdb76df6d356994b6c2e6
Varun <varun.2021@vitstudent.ac.in>
2025-12-03 16:46:46 +0530

    modified:   Src/apb_environment.sv

--- Src/apb_environment.sv
+++ Src/apb_environment.sv
@@ -18,32 +18,23 @@ class apb_env extends uvm_env;
     scoreboard = apb_scoreboard::type_id::create("scoreboard", this);
     subscriber = apb_subscriber::type_id::create("subscriber", this);
     
-    // Set agent configuration
-    uvm_config_db#(int)::set(this, "active_agent", "is_active", UVM_ACTIVE);
-    uvm_config_db#(int)::set(this, "passive_agent", "is_active", UVM_PASSIVE);
+    uvm_config_db#(uvm_active_passive_enum)::set(this, "active_agent", "is_active", UVM_ACTIVE);
+    uvm_config_db#(uvm_active_passive_enum)::set(this, "passive_agent", "is_active", UVM_PASSIVE);
     
     `uvm_info(get_type_name(), "Build phase complete", UVM_HIGH)
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    
-    // Connect active monitor to scoreboard and coverage
+
     active_agent.active_monitor.mon_port.connect(scoreboard.ip_fifo.analysis_export);
-    active_agent.active_monitor.mon_port.connect(subscriber.analysis_export);
-    
-    // Connect passive monitor to scoreboard
+    active_agent.active_monitor.mon_port.connect(subscriber.active_export);
     passive_agent.passive_monitor.mon_port.connect(scoreboard.op_fifo.analysis_export);
+    passive_agent.passive_monitor.mon_port.connect(subscriber.passive_export);
     
     `uvm_info(get_type_name(), "Connect phase complete", UVM_HIGH)
   endfunction
   
-  virtual function void end_of_elaboration_phase(uvm_phase phase);
-    super.end_of_elaboration_phase(phase);
-    `uvm_info(get_type_name(), "Topology:", UVM_LOW)
-    uvm_top.print_topology();
-  endfunction
-
 endclass
 
 
